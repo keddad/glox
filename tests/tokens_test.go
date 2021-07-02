ec2ac1cf8d7a84ca19577c64ac4586b7e9ea2788
keddad <keddad@yandex.ru>
2021-07-03 01:04:36 +0300

    Github Actions

--- tests/tokens_test.go
+++ tests/tokens_test.go
@@ -27,7 +27,7 @@ func TestSimple(t *testing.T) {
 
 func TestCorrectStr(t *testing.T) {
 	str := "var x = \"meow\";"
-	exp_tokens := []tokens.Token{{TokenType: 36, Lexeme: "var", InText: 1, Literal: nil}, {TokenType: 19, Lexeme: "x", InText: 1, Literal: nil}, {TokenType: 13, Lexeme: "=", InText: 1, Literal: nil}, {TokenType: 20, Lexeme: "meow", InText: 1, Literal: "meow"}, {TokenType: 8, Lexeme: ";", InText: 1, Literal: nil}}
+	exp_tokens := []tokens.Token{{TokenType: 36, Lexeme: "var", InText: 1, Literal: nil}, {TokenType: 19, Lexeme: "x", InText: 1, Literal: nil}, {TokenType: 13, Lexeme: "=", InText: 1, Literal: nil}, {TokenType: 20, Lexeme: "\"meow\"", InText: 1, Literal: "meow"}, {TokenType: 8, Lexeme: ";", InText: 1, Literal: nil}}
 
 	state := runtime.NewState()
 	gen_tokens := tokens.ScanTokens(str, &state)
